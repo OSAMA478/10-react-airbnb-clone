838a87bc6d6653c043e4a374174ab7dfaba321d6
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-30 00:55:24 +0530

    ✨ feat: dateRangePicker  added

--- components/Header.js
+++ components/Header.js
@@ -4,12 +4,38 @@ import { GoSearch } from "react-icons/go";
 import { BsGlobe } from "react-icons/bs";
 import { AiOutlineMenu } from "react-icons/ai";
 import { HiUserCircle } from "react-icons/hi";
+import { BsPersonFill } from "react-icons/bs";
+import { BsPeopleFill } from "react-icons/bs";
+
+import { DateRangePicker } from "react-date-range";
+
+import "react-date-range/dist/styles.css"; // main style file
+import "react-date-range/dist/theme/default.css"; // theme css file
 
 const brandLogo =
 	"https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png";
 
 const Header = () => {
 	const [searchInput, setSearchInput] = useState("");
+	const [numberOfPeople, setNumberOfPeople] = useState(1);
+	const [startDate, setStartDate] = useState(new Date());
+	const [endDate, setEndDate] = useState(new Date());
+
+	const handleSelect = (ranges) => {
+		setStartDate(ranges.selection.startDate);
+		setEndDate(ranges.selection.endDate);
+	};
+
+	const selectionRange = {
+		startDate: startDate,
+		endDate: endDate,
+		key: "selection",
+	};
+
+	const iconHandler = (event) => {
+		setNumberOfPeople(event.target.value);
+	};
+
 	return (
 		<header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-md md:px-10">
 			{/* left side */}
@@ -43,6 +69,29 @@ const Header = () => {
 					<HiUserCircle className="h-4" />
 				</div>
 			</div>
+			{searchInput && (
+				<div className="flex flex-col col-span-3 mx-auto mt-2 ">
+					<DateRangePicker
+						ranges={[selectionRange]}
+						minDate={new Date()}
+						rangeColors={["#F87171"]}
+						onChange={handleSelect}
+					/>
+					<div className="flex items-center mb-4 border-b">
+						<h2 className="flex-grow text-2xl font-semibold">
+							Number of Guest
+						</h2>
+						{numberOfPeople == 1 ? <BsPersonFill /> : <BsPeopleFill />}
+
+						<input
+							value={numberOfPeople}
+							type="number"
+							onChange={iconHandler}
+							className="w-12 pl-2 text-lg outline-none"
+						/>
+					</div>
+				</div>
+			)}
 		</header>
 	);
 };
