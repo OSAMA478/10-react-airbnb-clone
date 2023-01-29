6056cc68fd5728d3fb534ffe0bd676d0631a1028
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-30 01:31:44 +0530

    min value fix

--- components/Header.js
+++ components/Header.js
@@ -86,6 +86,7 @@ const Header = () => {
 						<input
 							value={numberOfPeople}
 							type="number"
+							min={1}
 							onChange={iconHandler}
 							className="w-12 pl-2 text-lg outline-none"
 						/>
