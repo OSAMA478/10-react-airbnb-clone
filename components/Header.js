883eaefa2cdbdd089786047b3768f55b38d5afe1
Mansoor Khan <mansoorKh1098@gmail.com>
2023-01-29 17:23:14 +0530

    💄 feat: banner component

--- components/Header.js
+++ components/Header.js
@@ -7,40 +7,40 @@ import { HiUserCircle } from "react-icons/hi";
 
 // const brandLogo = "https://links.papareact.com/qd3";
 const brandLogo =
-	"https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png";
+  "https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Airbnb_Logo_B%C3%A9lo.svg/2560px-Airbnb_Logo_B%C3%A9lo.svg.png";
 
 const Header = () => {
-	return (
-		<header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-md md:px-10">
-			{/* left side */}
-			<div className="relative flex items-center h-10 my-auto cursor-pointer">
-				<Image
-					className="object-contain object-left"
-					src={brandLogo}
-					alt="brand logo"
-					fill
-				/>
-			</div>
-			{/* middle side */}
-			<div className="flex items-center py-2 rounded-full md:border-2 md:shadow-sm">
-				<input
-					className="flex-grow w-full pl-5 text-sm text-gray-600 placeholder-gray-400 bg-transparent outline-none"
-					type="text"
-					placeholder="type your text"
-				/>
-				<GoSearch className="hidden w-8 h-8 p-2 text-white bg-red-400 rounded-full cursor-pointer md:inline-flex md:mx-2" />
-			</div>
-			{/* right side */}
-			<div className="flex items-center justify-end space-x-4 ">
-				<p className="hidden md:inline">become a host</p>
-				<BsGlobe className="h-4" />
-				<div className="flex items-center p-2 space-x-2 border-2 rounded-full">
-					<AiOutlineMenu className="h-4" />
-					<HiUserCircle className="h-4" />
-				</div>
-			</div>
-		</header>
-	);
+  return (
+    <header className="sticky top-0 z-50 grid grid-cols-3 p-5 bg-white shadow-md md:px-10">
+      {/* left side */}
+      <div className="relative flex items-center h-10 my-auto cursor-pointer">
+        <Image
+          className="object-contain object-left"
+          src={brandLogo}
+          alt="brand logo"
+          fill
+        />
+      </div>
+      {/* middle side */}
+      <div className="flex items-center py-2 rounded-full md:border-2 md:shadow-sm">
+        <input
+          className="flex-grow w-full pl-5 text-sm text-gray-600 placeholder-gray-400 bg-transparent outline-none"
+          type="text"
+          placeholder="type your text"
+        />
+        <GoSearch className="hidden w-8 h-8 p-2 text-white bg-red-400 rounded-full cursor-pointer md:inline-flex md:mx-2" />
+      </div>
+      {/* right side */}
+      <div className="flex items-center justify-end space-x-4 ">
+        <p className="hidden md:inline">become a host</p>
+        <BsGlobe className="h-4" />
+        <div className="flex items-center p-2 space-x-2 border-2 rounded-full">
+          <AiOutlineMenu className="h-4" />
+          <HiUserCircle className="h-4" />
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
--- next.config.js
+++ next.config.js
@@ -1,15 +1,21 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-	reactStrictMode: true,
-	images: {
-		remotePatterns: [
-			{
-				protocol: "https",
-				hostname: "upload.wikimedia.org",
-				port: "",
-				pathname: "/**",
-			},
-		],
-	},
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "upload.wikimedia.org",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "a0.muscache.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 };
