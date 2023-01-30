24953bebffd6b780dced517b883a5699f3679c9b
OSAMA SHAIKH <shaikhosama478@gmail.com>
2023-01-30 19:56:49 +0530

    ✨ feat: loader introduced

--- next.config.js
+++ next.config.js
@@ -1,28 +1,28 @@
 /** @type {import('next').NextConfig} */
 
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "upload.wikimedia.org",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "a0.muscache.com",
-        port: "",
-        pathname: "/**",
-      },
-      {
-        protocol: "https",
-        hostname: "links.papareact.com",
-        port: "",
-        pathname: "/**",
-      },
-    ],
-  },
+	reactStrictMode: true,
+	images: {
+		remotePatterns: [
+			{
+				protocol: "https",
+				hostname: "upload.wikimedia.org",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "a0.muscache.com",
+				port: "",
+				pathname: "/**",
+			},
+			{
+				protocol: "https",
+				hostname: "links.papareact.com",
+				port: "",
+				pathname: "/**",
+			},
+		],
+	},
 };
 // links.papareact.com
